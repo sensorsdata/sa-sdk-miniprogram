fda83111bc1bf4b693f5941e1f810b104ca5672d
日 月 <sheng.yonggeng@gmail.com>
2017-01-13 15:04:37 +0800

    Update sensorsdata_conf.js

--- sensorsdata_conf.js
+++ sensorsdata_conf.js
@@ -1,7 +1,7 @@
 var conf = {
     // 神策分析的全局变量名
     name: 'sensors',
-    // 神策分析数据接收地址,类似(http://xxx.cloud.sensorsdata.cn:8006/sa?project=gxq&token=312d2dfw21)
+    // 神策分析数据接收地址,类似(https://xxx.cloud.sensorsdata.cn:8006/sa?project=gxq&token=312d2dfw21)
     server_url: '',
     // 传入的字符串最大长度限制
     max_string_length: 200,
