3c866d169df79837a3856d7151d6bf8f946f2106
日 月 <sheng.yonggeng@gmail.com>
2017-01-13 15:05:22 +0800

    Update sensorsdata_conf.js

--- sensorsdata_conf.js
+++ sensorsdata_conf.js
@@ -2,6 +2,7 @@ var conf = {
     // 神策分析的全局变量名
     name: 'sensors',
     // 神策分析数据接收地址,类似(https://xxx.cloud.sensorsdata.cn:8006/sa?project=gxq&token=312d2dfw21)
+    // 注意请在微信里设置这个域名可以访问
     server_url: '',
     // 传入的字符串最大长度限制
     max_string_length: 200,
