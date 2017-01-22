610cf20ea66be9ee4ba8c7589f74ae080f4d3192
shengyonggen <spiritpanda@sina.com>
2017-01-22 17:07:01 +0800

    增加register

--- sensorsdata_conf.js
+++ sensorsdata_conf.js
@@ -1,13 +1,14 @@
 var conf = {
     // 神策分析的全局变量名
     name: 'sensors',
-    // 神策分析数据接收地址,类似(https://xxx.cloud.sensorsdata.cn:8006/sa?project=gxq&token=312d2dfw21)
-    // 注意请在微信里设置这个域名可以访问
-    server_url: '',
+    // 神策分析数据接收地址,类似(http://xxx.cloud.sensorsdata.cn:8006/sa?project=gxq&token=312d2dfw21)
+    server_url: 'http://xxx.cloud.sensorsdata.cn:8006/sa',
     // 传入的字符串最大长度限制
-    max_string_length: 200,
+    max_string_length: 100,
     // 发送事件的时间使用客户端时间还是服务端时间
-    use_client_time: false
+    use_client_time: false,
+    // 选择需要
+    register: {}
 
 };
 
