bd697103d84770792879699e0107d69c139deacc
Li Xiang <1656105600@qq.com>
2017-04-10 17:37:10 +0800

    pageOnshow 自定义参数

--- sensorsdata_conf.js
+++ sensorsdata_conf.js
@@ -8,8 +8,13 @@ var conf = {
     // 发送事件的时间使用客户端时间还是服务端时间
     use_client_time: false,
     // 选择需要
-    register: {}
-
+    register: {},
+    // 如果需要向 pageOnshow 方法中添加自定义参数（ sa.track 方法中添加参数)则保留 onshow 方法，否则删除下面方法即可。
+    onshow:function(sa,router){
+        sa.track('$MPViewScreen', {
+            $url : router
+        });
+    }
 };
 
 module.exports = conf;
