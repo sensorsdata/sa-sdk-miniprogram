02a34c3e44e90c7de98e650ffb9459dae10748dc
shengyonggen <shengyonggen@sensorsdata.cn>
2021-03-13 18:25:08 +0800

    Release 1.14.6

---
+++ sensorsdata.min.d.ts
@@ -0,0 +1,19 @@
+declare namespace sensors {
+  function init(): void;
+  function track(e: string, p?: object): void;
+  function setPara (p: object): void;
+
+  function identify(id: string, isSave?: boolean): void;
+  function login(id: string): void;
+  function logout(isChangeId?: boolean): void;
+
+  function setOnceProfile(prop: object): void;
+  function setProfile(prop: object): void;
+  function appendProfile(prop: object): void;
+  function incrementProfile(prop: object): void;
+
+  function registerApp(prop: object): void;
+  function clearAppRegister(list: Array<string>): void;
+}
+
+export default sensors;
